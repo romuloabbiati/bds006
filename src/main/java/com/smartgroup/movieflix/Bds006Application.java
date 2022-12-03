33963164c865e2b31f9c2633a92a88b3fcf77e22
romuloabbiati <rhabbiati@hotmail.com>
2022-12-03 09:49:54 +0000

    first commit

---
+++ src/main/java/com/smartgroup/movieflix/Bds006Application.java
@@ -0,0 +1,13 @@
+package com.smartgroup.movieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bds006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bds006Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartgroup/movieflix/Bds006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgroup.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bds006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
