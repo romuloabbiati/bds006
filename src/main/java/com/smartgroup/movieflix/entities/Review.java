c38a1ff6a91ad43efe39a618590d6f1be7673b22
romuloabbiati <rhabbiati@hotmail.com>
2022-12-03 10:19:23 +0000

    Entities: genre movie review role user

---
+++ src/main/java/com/smartgroup/movieflix/entities/Review.java
@@ -0,0 +1,63 @@
+package com.smartgroup.movieflix.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
+public class Review implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String text;
+	
+	public Review() {}
+
+	public Review(Long id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
