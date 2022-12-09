97db0cd74343759ec788accbd751fa71e45aac24
romuloabbiati <rhabbiati@hotmail.com>
2022-12-09 09:13:03 +0000

    movieAllFieldsDTO, movieService.findById()

--- src/main/java/com/smartgroup/movieflix/controllers/MovieController.java
+++ src/main/java/com/smartgroup/movieflix/controllers/MovieController.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smartgroup.movieflix.dto.MovieAllFieldsDTO;
 import com.smartgroup.movieflix.dto.MovieDTO;
 import com.smartgroup.movieflix.services.MovieService;
 
@@ -18,6 +20,12 @@ public class MovieController {
 
 	@Autowired
 	private MovieService movieService;
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<MovieAllFieldsDTO> findById(@PathVariable Long id) {
+		MovieAllFieldsDTO movieDTO = movieService.findById(id);
+		return ResponseEntity.ok(movieDTO);
+	}
 	
 	@GetMapping
 	public ResponseEntity<Page<MovieDTO>> findByGenre(
--- src/main/java/com/smartgroup/movieflix/services/MovieService.java
+++ src/main/java/com/smartgroup/movieflix/services/MovieService.java
@@ -1,20 +1,31 @@
 package com.smartgroup.movieflix.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.smartgroup.movieflix.dto.MovieAllFieldsDTO;
 import com.smartgroup.movieflix.dto.MovieDTO;
 import com.smartgroup.movieflix.entities.Movie;
 import com.smartgroup.movieflix.repositories.MovieRepository;
+import com.smartgroup.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
 
 	@Autowired
 	private MovieRepository movieRepository;
+	
+	@Transactional(readOnly = true)
+	public MovieAllFieldsDTO findById(Long id) {
+		Optional<Movie> movieOptional = movieRepository.findById(id);
+		Movie movie = movieOptional.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
+		return new MovieAllFieldsDTO(movie);
+	}
 	
 	@Transactional(readOnly = true)
 	public Page<MovieDTO> findByGenre(Long genreId, Pageable pageable) {
